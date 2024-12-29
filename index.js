b3b9afc4f2aa47235073eb6396408c66e2fe27b5
Judicael Tombo <44143598+jtombo45@users.noreply.github.com>
2024-12-28 21:20:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
       // Controller
       let isPlay = false;
+      // SVG
+      var svgPlayElement = $('<svg id="fi_727245" width="25px" height="25px" enable-background="new 0 0 320.001 320.001" viewBox="0 0 320.001 320.001" xmlns="http://www.w3.org/2000/svg"><path d="m295.84 146.049-256-144c-4.96-2.784-11.008-2.72-15.904.128-4.928 2.88-7.936 8.128-7.936 13.824v288c0 5.696 3.008 10.944 7.936 13.824 2.496 1.44 5.28 2.176 8.064 2.176 2.688 0 5.408-.672 7.84-2.048l256-144c5.024-2.848 8.16-8.16 8.16-13.952s-3.136-11.104-8.16-13.952z"></path><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>');
+  var svgPauseElement = $('<svg id="fi_727245" width="25px" height="25px" viewBox="-45 0 327 327" width="327pt" xmlns="http://www.w3.org/2000/svg" id="fi_1214679"><path d="m158 0h71c4.417969 0 8 3.582031 8 8v311c0 4.417969-3.582031 8-8 8h-71c-4.417969 0-8-3.582031-8-8v-311c0-4.417969 3.582031-8 8-8zm0 0"></path><path d="m8 0h71c4.417969 0 8 3.582031 8 8v311c0 4.417969-3.582031 8-8 8h-71c-4.417969 0-8-3.582031-8-8v-311c0-4.417969 3.582031-8 8-8zm0 0"></path></svg>')
+  //Set button svg
+  $("#ctrl-bttn").append(svgPlayElement);
       // Canvas setup
       const canvas = document.getElementById('circleCanvas');
       const ctx = canvas.getContext('2d');
@@ -113,16 +118,32 @@ $(document).ready(function () {
 
       // Initial draw to display the circle and static timer
       draw();
+  
+      // Event listeners for Reset
+      $('#reset-bttn').on('click', function () {
+        stopAnimation(); // Stop any ongoing animation or timer
+        timerValue = 0; // Reset the timer value to 0
+        angle = 0; // Reset the angle to its initial position
+        draw(); // Redraw the canvas with the reset values
+
+        // Reset the Play/Pause button to the "Play" state
+        isPlay = false; 
+        $("#ctrl-bttn").empty(); 
+        $("#ctrl-bttn").append(svgPlayElement);
+    });
+
 
       // Event listeners for Play and Pause buttons
-      $('#play').on('click', function(){
+      $('#ctrl-bttn').on('click', function(){
         if(isPlay){
           stopAnimation();
-          $(this).text('play');
+          $("#ctrl-bttn").empty();
+          $("#ctrl-bttn").append(svgPlayElement);
         }
         else{
           startAnimation();
-          $(this).text('pause');
+          $("#ctrl-bttn").empty();
+          $("#ctrl-bttn").append(svgPauseElement);
         }
         isPlay = !isPlay;
         
